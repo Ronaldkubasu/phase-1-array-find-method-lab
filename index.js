4350ba34f8c14516f36e023765b1626bffbbf417
Ronald kubasu <ronald.kubasu@student.moringaschool.com>
2022-08-20 22:00:55 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+let superbowlWin = arrayOfObjects=> {
+    let found = arrayOfObjects.find(element=>element.result=='W')
+      if (found){
+          return found.year
+      }
+      else{
+          return undefined
+      }
+  }
+  
+  const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  { year: "2010", result: "W"},
+  ]
+  
+  console.log(superbowlWin(record))
